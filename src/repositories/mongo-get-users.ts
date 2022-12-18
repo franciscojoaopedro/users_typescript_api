42e826508000ac434e39350046c246d0da7f7471
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-12-18 19:41:57 +0000

    feat ensure getUseRepository get user MongoDB

--- src/repositories/mongo-get-users.ts
+++ src/repositories/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUserRepository } from '../controllers/get-users/protocols'
+import { MongoClient } from '../database/mongo'
 import { User } from '../models/user'
 
 export class MongoGetUsersRepository implements IGetUserRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: 'Francisco',
-        lastName: 'Pedro',
-        email: 'francisco@gmail.com',
-        password: '123',
-      },
-    ]
+    const users = await MongoClient.db
+      .collection<Omit<User, 'id'>>('users')
+      .find({})
+      .toArray()
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }))
   }
 }
